ffc267c2427b69ab60434504418f162d6ae0ba8c
vxntvra senpai <scratch44tutorials@gmail.com>
2020-12-30 11:16:37 +0100

    basic items added

---
+++ items.py
@@ -0,0 +1,15 @@
+helmets = {
+    "Rusty Helmet" : "2 Defense\n\nA rusty helmet found in the basement of the traveler. \n\nSeems to give some sort of defense against weak attacks."
+}
+
+chestplates = {
+    "Rusty Chestplate" : "4 Defense\n\nA rusty chestplate found in the basement of the traveler. \n\nWill serve as a defense against the weak attacks, \nbut not really reliable"
+}
+
+boots = {
+    "Rusty Leather Boots" : "2 Defense\n\nRusty leather boots found in the basement of the traveler. \nWill endure some attacks, but only the weak ones."
+}
+
+weapons = {
+    "Rusty Dagger" : "6 Damage\n\nA rusty Dagger found in the basement of the traveler. \n Will do common damage to a weak enemy, but definitly not a weapon of choice."
+}
---
+++ player.py
@@ -0,0 +1,12 @@
+class Player:
+    def __init__(self, name, gender, type):
+        self.name = name
+        self.gender = gender
+        self.type = type
+    health = 100
+    helmet = ""
+    chestplate = ""
+    boots = ""
+
+    weapon
+    
